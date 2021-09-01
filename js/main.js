ecfd1d598896800329f8c15ed21863747217db7e
monipeny <mlamasrial@gmail.com>
2021-09-01 10:11:46 +0200

    create function checkNumber

--- js/main.js
+++ js/main.js
@@ -7,3 +7,29 @@ const guessSubmit = document.querySelector('.js_guessSubmit');
 
 const clue = document.querySelector('.js_textClue');
 const attempts = document.querySelector('.js_attempts');
+
+function checkNumber() {
+    let userGuess = Number(guess.value);
+    if (userGuess === randomNumber) {
+        clue.innerHTML = 'Has ganado campeona!!!';
+    } else if (userGuess > randomNumber) {
+        clue.innerHTML = 'Demasiado alto.';
+    } else if (userGuess < randomNumber) {
+        clue.innerHTML = 'Demasiado bajo.';
+    } else {
+        clue.innerHTML = `El número debe estar
+        entre 1 y 100.`;
+    }
+}
+
+function countAttempts() {
+
+}
+
+function handleSubmitBtn(event) {
+    event.preventDefault();
+    checkNumber();
+    console.log(randomNumber);
+}
+
+guessSubmit.addEventListener('click', handleSubmitBtn);
