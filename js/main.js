166bc011df7355e1fd1c7eed18c7bc8d2556f56b
monipeny <mlamasrial@gmail.com>
2021-09-01 09:41:59 +0200

    create variables in main.js

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@
 
 let randomNumber = Math.ceil(Math.random() * 100);
 
-const guess = document.querySelector('.js_guess');
+const guess = document.querySelector('.js_guessField');
+const guessSubmit = document.querySelector('.js_guessSubmit');
+
+const clue = document.querySelector('.js_textClue');
+const attempts = document.querySelector('.js_attempts');
